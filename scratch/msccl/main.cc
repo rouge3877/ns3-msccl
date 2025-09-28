8ac75c4148e4e2c0abc369cb6055596dae6cac74
Yuxuan Li <rouge_3877@outlook.com>
2025-09-28 14:43:01 +0800

    💩 Add config manager and main sim logic for MSCCL
    feat:
    - Introduced a new configuration management system in config.h to handle various data types including basic types, arrays, and hash maps.
    - Implemented a class structure for configuration variables and methods to set and retrieve configuration values.
    - Added a GroupConfig class to manage configurations for groups of nodes.
    - Created main.cc to initialize the simulation environment, parse command line arguments, and set up the network.
    - Integrated logging and message handling for RDMA queue pairs.
    - Ensured compatibility with MTP and MPI interfaces for distributed simulations.
    
    problem now:
    - concurrency problem if enable-mtp
    - large message size bring out high sim time
    - higher interval between messages, higher sim time ( see
      `QpReuseNetwork` with time in 2 4 6 8)

---
+++ scratch/msccl/main.cc
@@ -0,0 +1,137 @@
+#include <execinfo.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/allstack-module.h"
+
+#ifdef NS3_MTP
+#include "ns3/mtp-interface.h"
+#endif
+#ifdef NS3_MPI
+#include <mpi.h>
+#include "ns3/mpi-interface.h"
+#endif
+
+#include "common.h"
+
+using namespace std;
+using namespace ns3;
+
+extern uint32_t node_num, switch_num, link_num, trace_num, nvswitch_num, gpus_per_server;
+extern tinyxml2::XMLDocument algo_xml_doc;
+extern std::unordered_map<uint32_t, unordered_map<uint32_t, uint16_t>> portNumber;
+extern double simulator_start_time;
+extern double simulator_stop_time;
+
+
+void my_qp_finish(FILE *fout, Ptr<RdmaQueuePair> q)
+{
+    uint32_t sid = ip_to_node_id(q->sip), did = ip_to_node_id(q->dip);
+#ifdef NS3_MTP
+    MtpInterface::explicitCriticalSection cs;
+#endif
+    Ptr<Node> dstNode = n.Get(did);
+    Ptr<RdmaDriver> rdma = dstNode->GetObject<RdmaDriver>();
+    rdma->m_rdma->DeleteRxQp(q->sip.Get(), q->m_pg, q->sport);
+
+    NS_LOG_INFO("QP FINISH: src " << sid << " did " << did << " port " << q->sport);
+
+    cancel_monitor();
+#ifdef NS3_MTP
+    cs.ExitSection();
+#endif
+}
+
+void my_send_finish(FILE *fout, Ptr<RdmaQueuePair> q)
+{
+    // Seems that the occasion to call this function is error-prone !!! (rouge)
+    // rouge: this function is called when send is finished (without ack) ! (rouge after)
+#ifdef NS3_MTP
+    MtpInterface::explicitCriticalSection cs;
+#endif
+    // Currently do nothing
+    // uint32_t sid = ip_to_node_id(q->sip), did = ip_to_node_id(q->dip);
+#ifdef NS3_MTP
+    cs.ExitSection();
+#endif
+}
+
+void my_message_finish(FILE *fout, Ptr<RdmaQueuePair> q, uint64_t msgSize)
+{
+#ifdef NS3_MTP
+    MtpInterface::explicitCriticalSection cs;
+#endif
+    uint32_t sid = ip_to_node_id(q->sip), did = ip_to_node_id(q->dip);
+    uint64_t base_rtt = pairRtt[sid][did], b = pairBw[sid][did];
+    uint32_t packet_payload_size =
+        get_config_value_ns3<uint64_t>("ns3::RdmaHw::Mtu");
+    uint64_t size = msgSize;
+    uint32_t total_bytes = size +
+                           ((size - 1) / packet_payload_size + 1) *
+                               (CustomHeader::GetStaticWholeHeaderSize() -
+                                IntHeader::GetStaticSize()); // translate to the minimum bytes
+                                                             // required (with header but no INT)
+    uint64_t standalone_fct = base_rtt + total_bytes * 8000000000lu / b;
+    fprintf(
+        fout,
+        "%08x %08x %u %u %lu %lu %lu %lu\n",
+        q->sip.Get(),
+        q->dip.Get(),
+        q->sport,
+        q->dport,
+        size,
+        q->startTime.GetTimeStep(),
+        (Simulator::Now() - q->startTime).GetTimeStep(),
+        standalone_fct);
+    fflush(fout);
+
+    NS_LOG_INFO("MESSAGE FINISH: src " << sid << " did " << did << " port " << q->sport << " size " << size);
+    // Ptr<Node> dstNode = n.Get(did);
+    // Ptr<RdmaDriver> rdma = dstNode->GetObject<RdmaDriver>();
+    // rdma->m_rdma->DeleteRxQp(q->sip.Get(), q->m_pg, q->sport);
+#ifdef NS3_MTP
+    cs.ExitSection();
+#endif
+}
+
+int main(int argc, char *argv[])
+{
+#ifdef NS3_MTP
+    MtpInterface::Enable(16);
+#endif
+#ifdef NS3_MPI
+    ns3::MpiInterface::Enable(&argc, &argv);
+#endif
+
+    LogComponentEnable("MSCCL", LOG_LEVEL_INFO);
+
+    CommandLine cmd;
+    cmd.Parse(argc, argv);
+    if (!ReadConf(argc, argv))
+        return -1;
+    SetConfig();
+    SetupNetwork(my_qp_finish, my_send_finish, my_message_finish);
+    fflush(stdout);
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    NS_LOG_INFO(" //////////////////// Start Simulation. //////////////////// ");
+    //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    Simulator::Run();
+    Simulator::Stop(Seconds(simulator_stop_time));
+    Simulator::Destroy();
+
+    return 0;
+}
